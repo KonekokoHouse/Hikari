5fa34dbb278d9181077a9fea60ede91c232968e5
L3MonKe <hhcrystal2008@gmail.com>
2026-03-08 01:47:56 +0800

    Loader and verification server. (#5)
    * idk
    
    * b
    
    * bbb
    
    * 我不行了这个真的有点强，然后还得在lumin本体里加点东西，不然dump直接crack666
    
    * b
    
    * BB
    
    * b
    
    * b
    
    * b
    
    * b
    
    * b
    
    * bbbbbb
    
    * b

--- src/main/java/com/github/lumin/Lumin.java
+++ src/main/java/com/github/lumin/Lumin.java
@@ -1,11 +1,10 @@
 package com.github.lumin;
 
 import com.github.lumin.managers.Managers;
+import com.github.lumin.utils.AuthUtils;
 import com.mojang.logging.LogUtils;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
@@ -83,11 +82,11 @@ public class Lumin {
 
     public static int skipTicks;
 
-    public Lumin(IEventBus modEventBus, ModContainer modContainer) {
-    }
-
     @SubscribeEvent
-    static void onClientSetup(FMLClientSetupEvent event) {
+    private static void onClientSetup(FMLClientSetupEvent event) {
+
+        AuthUtils.checkConnection();
+
         LOGGER.info("Welcome to Lumin, Meow~");
 
         Managers.initManagers();
@@ -98,6 +97,7 @@ public class Lumin {
         }));
 
         Lumin.LOGGER.info("Lumin has loaded successfully, Meow~");
+
     }
 
 }
